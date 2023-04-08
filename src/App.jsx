f07631fab959d05d9822b0f206c1c0065b69288a
md areeb uddin <mdareeb.uddin@truelancer.com>
2023-04-08 11:32:53 +0530

    react router setup with file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import react, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useSelector, useDispatch } from "react-redux";
 
 import { fetchDataFromApi } from "./utils/api";
 import { getUrlConfiguration } from "./store/homeSlice";
+
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Home from "./pages/home/Home";
+import Details from './pages/details/Details'
+import SearchResult from './pages/searchDetail/SearchDetail'
+import Explore from './pages/explore/Explore'
+import PageNotFound from './pages/404/PageNotFound'
 
 function App() {
   const dispatch = useDispatch();
@@ -17,7 +26,17 @@ function App() {
     });
   };
 
-  return <div className="App">CINEMAGIC</div>;
+  return(
+    <BrowserRouter>
+      <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
